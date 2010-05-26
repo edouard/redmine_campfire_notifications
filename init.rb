c8f67b2314950db6c076382a8dad2db08220d576
Édouard Brière <edouard.briere@gmail.com>
2010-05-26 22:12:20 +0200

    Bump to 0.0.2

--- init.rb
+++ init.rb
@@ -5,5 +5,5 @@ Redmine::Plugin.register :redmine_campfire_notifications do
   name 'Redmine Campfire Notifications plugin'
   author 'Édouard Brière'
   description 'A plugin to display issue modifications to a Campfire room'
-  version '0.0.1'
+  version '0.0.2'
 end
